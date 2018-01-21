91b84b4b7f3563688bd03288ac96bc535b4b6392
Abraham George <abraham.george.j@gmail.com>
2018-01-21 12:44:26 +0530

    Verifying test data and restructuring folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import App from './components/App';
+import data from './testData.json';
 
+console.log(data);
 ReactDOM.render(
     <App />,
     document.getElementById('root')
